4a40f9e4728af7cdc89b3710454f0ee82bd278ef
stem-networks <stem.networks.india@gmail.com>
2025-11-19 11:34:20 +0530

    Map and Whatsapp Chat design updated -Srilakshmi2

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -129,7 +129,7 @@ const Header: React.FC<HeaderProps> = ({ generalData, registerData }) => {
         {/* Header Upper */}
         <div className="header-upper">
           <div className="auto-container">
-            <div style={{ justifyContent: "space-between", display: "flex" }}>
+            <div style={{ justifyContent: "space-between", display: "flex", alignItems: "center" }}>
               <div className="logo-main-block">
                 <Link
                   href="/"
